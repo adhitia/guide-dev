243fcc8f8652db77b7b0905e5b93f58d1839ac4d
Nikita Rybak <nikita.rybak@gmail.com>
2010-07-29 02:56:21 -0300

    environment properties

--- config/initializers/environment_specific.rb
+++ config/initializers/environment_specific.rb
@@ -1,7 +1,6 @@
 puts "initializing local environment in environment_specific.rb"
 
 WEB_ROOT = 'http://localhost:3000'
-
 FACEBOOK_APP_ID = 'e8a82604ae771543db615e26aeb68c59'
 FACEBOOK_APP_SECRET = 'b2c7f0025850a6d3fd88c56a93ab8e0c'
 
