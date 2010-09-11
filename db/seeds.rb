7e2ca876b4ce6f3103932633f4884c1425f52ec1
Nikita Rybak <nikita.rybak@gmail.com>
2010-09-11 00:49:03 -0300

    nowrap fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@
 #include ApplicationHelper
 # why fucking error?
 
-# http://cpanel.brazilwide.com.br
-# gongon6442
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
