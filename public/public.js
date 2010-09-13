663d09797335b8e06aeaf1503a450adb475ce453
Nikita Rybak <nikita.rybak@gmail.com>
2010-09-12 23:23:19 -0300

    trying to load qtip

--- public/public.js
+++ public/public.js
@@ -55,12 +55,12 @@ if (!window._guiderer) {
 //                    var el = $(res);
                     target.html('');
                     target.append(res);
-                    alert('0 ' + target.find('.guiderer-resource').length);
-                    target.find('.guiderer-resource').load(function() {
-                        alert(1);
+//                    alert('0 ' + target.find('.guiderer-resource').length);
+//                    target.find('.guiderer-resource').load(function() {
+//                        alert(1);
 //                        _guiderer.init(el);
                         _guiderer.init(target);
-                    });
+//                    });
                 },
                 error: function(r, s, e) {
                     target.html('error has occurred');
