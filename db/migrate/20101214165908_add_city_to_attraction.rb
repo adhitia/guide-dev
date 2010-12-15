22779100043c5f86e268f964a6405dc678c549f7
Nikita Rybak <nikita.rybak@gmail.com>
2010-12-15 17:52:50 -0200

    heroku fix

--- db/migrate/20101214165908_add_city_to_attraction.rb
+++ db/migrate/20101214165908_add_city_to_attraction.rb
@@ -2,8 +2,10 @@ class AddCityToAttraction < ActiveRecord::Migration
   def self.up
     add_column :attractions, :city_id, :integer
     Tip.all.each do |tip|
-      tip.attraction.city_id = tip.calendar.location_id
-      tip.attraction.save!
+      if !tip.attraction.nil?
+        tip.attraction.city_id = tip.calendar.location_id
+        tip.attraction.save!
+      end
     end
   end
 
